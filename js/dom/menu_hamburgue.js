3eb7ba424d2d417bf1f225e2205ddc9cba654ab8
edgarunipacifico <edgarunipacifico@gmail.com>
2021-05-22 15:55:46 -0500

    first commit

---
+++ js/dom/menu_hamburgue.js
@@ -0,0 +1,18 @@
+ export default function hamburgerMenu( panelBtn, panel, menuLink){
+     const d =document;
+        
+     d.addEventListener("click",(e)=> {
+        
+        if(e.target.matches("panelBtn") || e.target.matches(`${panelBtn} *`)){// si el evento que genero el click concide con el panelboton 0cual quier hijo que este dentro del  boton
+          
+            d.querySelector(panel).classList.toggle("is-active");
+            d.querySelector(panelBtn).classList.toggle("is-active");
+        }
+        
+        if(e.target.matches(menuLink)){
+            d.querySelector(panel).classList.remove("is-active");
+            d.querySelector(panelBtn).classList.remove("is-active");
+        }
+     });
+
+ }
